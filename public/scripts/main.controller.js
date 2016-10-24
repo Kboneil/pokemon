7cbb48f489b279fbd273c48d2b197d4c7988f465
Kelly ONeil <kelly.brianne.oneil@gmail.com>
2016-10-24 13:45:13 -0500

    started adding catch feature

--- public/scripts/main.controller.js
+++ public/scripts/main.controller.js
@@ -1,10 +1,16 @@
 angular.module('routeApp')
 .controller('MainController', MainController);
 
-function MainController () {
+function MainController ($http) {
   var main=this;
 
-  main.showStats = function () {
-
-        };
-      };
+  main.catch = function () {
+    console.log('inside catch');
+      main.pokemon = 'hide';
+      main.catch = 'catch';
+  };
+  main.reset = function () {
+    main.pokemon = '';
+    main.catch = 'pokeballCatch';
+  };
+}
